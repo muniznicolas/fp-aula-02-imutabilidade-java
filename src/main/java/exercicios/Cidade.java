bcbe4440aff19da550d8918eb2024280746d6fc6
muniznicolas <nicolasaraujomuniz@gmail.com>
2025-10-20 15:27:02 -0300

    Implementa Cidade, CidadeRecord, PessoaRecordShallow e PessoaRecord

---
+++ src/main/java/exercicios/Cidade.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Cidade {
+    private String nome;
+
+    public Cidade(String nome) {
+        this.nome = nome;
+    }
+
+    public Cidade() { }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
